a35acf36ae1adcdef0fc100958f921f9e1fa8405
Zerq <53876273+ZerqGitz@users.noreply.github.com>
2021-05-26 19:27:23 +0300

    Delete Full Game directory

--- Full Game/scripts/assets/music_player.py
+++
@@ -1,8 +0,0 @@
-import pygame, os
-
-def play_music(location_list: list, loop=0):
-    location = location_list[0]
-    for location_entry in location_list[1:]:
-        location = os.path.join(location, location_entry)
-    pygame.mixer.music.load(location)
-    return pygame.mixer.music.play(loop)
